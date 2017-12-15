dfe6aa164b8dcbadc9982a822bd497d69be70a88
huangjiedragon <huangjiedragon@126.com>
2017-12-15 18:33:52 +0800

    Tips组件

--- src/euiComponent/Tips.ts
+++ src/euiComponent/Tips.ts
@@ -11,8 +11,9 @@ class Tips {
 		return Tips.instance;
 	}
 
+
 	private layer:egret.DisplayObjectContainer;
-	//private pool:Array<TipItem>;
+	private pool:Array<TipItem>;
 	private queue:Array<number>;
 
 	public setLayer(layer:egret.DisplayObjectContainer): void{
@@ -23,5 +24,29 @@ class Tips {
 
 	public static show(msg:string):void {
 		//Tips.getInstance().initView(msg);
+	}
+}
+
+class TipItem extends egret.Sprite{
+	public constructor(){
+		super();
+		this.init();
+	}
+
+	private textField:egret.TextField;
+	private bg:egret.Texture;
+
+	private init():void{
+		this.width = egret.MainContext.instance.stage.stageWidth * 0.6;
+		this.textField = new egret.TextField();
+		this.textField.size = 26;
+		this.textField.bold = true;
+		this.textField.textColor = 0xffffff;
+		this.textField.multiline = true;
+		this.textField.wordWrap = true;
+		this.textField.textAlign = egret.HorizontalAlign.CENTER;
+		this.textField.width = egret.MainContext.instance.stage.stageWidth * 5;
+		this.textField.y = 10;
+		this.addChild(this.textField);
 	}
 }
